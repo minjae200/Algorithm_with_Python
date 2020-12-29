a35d72c95dc62a83f0ae8ac6e47cc6364d85f661
minjae.choi <chlalswo200@naver.com>
2020-12-29 19:16:49 +0900

    [Sort] 부품 찾기

---
+++ 연습/13_부품찾기.py
@@ -0,0 +1,25 @@
+n = int(input())
+array = [0] * 1000000
+
+for i in input().split():
+    array[int(i)] = 1
+
+m = int(input())
+x = list(map(int, input().split()))
+for i in x:
+    if array[i] == 1:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
+
+"""
+n = int(input())
+array = set(map(int, input().split()))
+m = int(input())
+x = list(map(int, input().split()))
+for i in x:
+    if i in array:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
+"""
