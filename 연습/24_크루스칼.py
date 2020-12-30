4b59b5f42c25ebc2d61337bf304afdd823d9ce41
minjae.choi <chlalswo200@naver.com>
2020-12-30 20:05:10 +0900

    [MST] 크루스칼

---
+++ 연습/24_크루스칼.py
@@ -0,0 +1,34 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, x, y):
+    x = find_parent(parent, x)
+    y = find_parent(parent, y)
+
+    if x < y:
+        parent[y] = x
+    else:
+        parent[x] = y
+
+v, e = map(int, input().split())
+parent = [0] * (v+1)
+edges = []
+
+for i in range(1, v+1):
+    parent[i] = i
+
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+
+edges.sort()
+result = 0
+for edge in edges:
+    cost, a, b = edge
+
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+print(result)
