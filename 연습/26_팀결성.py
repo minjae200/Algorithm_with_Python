959376affac6b044a4974a3dd7530ad348bfc1f5
minjae.choi <chlalswo200@naver.com>
2020-12-30 22:16:57 +0900

    [Graph] 팀 결성

---
+++ 연습/26_팀결성.py
@@ -0,0 +1,29 @@
+n, m = map(int, input().split())
+parent = [0] * (n+1)
+for i in range(1, n+1):
+    parent[i] = i
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, x, y):
+    x = find_parent(parent, x)
+    y = find_parent(parent, y)
+
+    if x < y:
+        parent[y] = x
+    else:
+        parent[x] = y
+
+for _ in range(m):
+    oper, a, b = map(int, input().split())
+
+    if oper == 0:
+        union_parent(parent, a, b)
+    elif oper == 1:
+        if find_parent(parent, a) == find_parent(parent, b):
+            print("YES")
+        else:
+            print("NO")
