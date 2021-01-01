7d9160318d06758f1d9d92124419af2f4d66b702
minjae.choi <chlalswo200@naver.com>
2021-01-01 13:37:22 +0900

    [Implementation] 문자열 압축

---
+++ 실전/09_문자열압축.py
@@ -0,0 +1,23 @@
+#https://programmers.co.kr/learn/courses/30/lessons/60057?language=python3
+
+def solution(s):
+    answer = len(s)
+    for step in range(1, len(s)//2+1):
+        current_string = s[:step]
+        count = 1
+        compression = ""
+        for j in range(step, len(s), step):
+            next_string = s[j:j+step]
+            if current_string == next_string:
+                count += 1
+            else:
+                compression += str(count) + current_string if count >= 2 else current_string
+                current_string = next_string
+                count = 1
+        compression += str(count) + current_string if count >= 2 else current_string
+        answer = min(answer, len(compression))
+    return answer
+
+
+s = str(input())
+print(solution(s))
