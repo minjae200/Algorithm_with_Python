c76ac0314f1c6fa5d0509227f008e1e3d59d341d
minjae.choi <chlalswo200@naver.com>
2021-01-20 21:47:45 +0900

    [DynamicProgramming] 병사 배치하기

---
+++ 실전/34_병사배치하기.py
@@ -0,0 +1,9 @@
+n = int(input())
+array = list(map(int, input().split()))
+dp = [1] * n
+
+for i in range(1, n):
+    for j in range(i):
+        if array[j] > array[i]:
+            dp[i] = max(dp[j]+1, dp[i])
+print(n-max(dp))
